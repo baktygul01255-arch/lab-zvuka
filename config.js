63356218029a38b723231badbdb8dcd351d2207b
baktygul01255-arch <baktygul01255@gmail.com>
2026-09-20 10:55:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Вставьте между кавычками адрес веб-приложения Apps Script (он заканчивается на /exec).
 // Пока адрес пустой, приложение работает без облака: проект хранится только на этом устройстве.
 window.LAB_CONFIG = {
-  apiUrl: ''
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwMVxQUtx1ysRU4dkAzGGi1zfb1RDMQxq-r7TPplZ0WpooRQ5sVa49xRsyuwQ-r7ImDyg/exec'
 };
